21e60f8e39a239fd29dcd8ad768898db08b2e6eb
Jesus Figueroa <jsusfigueroa@ciencias.unam.mx>
2026-04-30 12:35:55 -0600

    Se elimina el atributo tables ya que resulto innecesario, además se refactoriza el metodo para compliar a sql

--- src/sql_query.rs
+++ src/sql_query.rs
@@ -1,11 +1,8 @@
-use std::collections::HashSet;
-
 use crate::expression::Expr;
 
 pub(crate) struct SqlQuery {
-    query: String,
-    params: Vec<String>,
-    tables: HashSet<String>,
+    pub query: String,
+    pub params: Vec<String>,
 }
 
 impl SqlQuery {
@@ -14,12 +11,10 @@ impl SqlQuery {
             Expr::And(left, right) => {
                 let mut left_sql = SqlQuery::compile_to_sql(left)?;
                 let mut right_sql = SqlQuery::compile_to_sql(right)?;
-                left_sql.tables.extend(right_sql.tables);
                 left_sql.params.append(&mut right_sql.params);
                 Ok(SqlQuery {
                     query: format!("{} AND {}", left_sql.query, right_sql.query),
                     params: left_sql.params,
-                    tables: left_sql.tables,
                 })
             }
             Expr::Not(ex) => {
@@ -27,31 +22,21 @@ impl SqlQuery {
                 Ok(SqlQuery {
                     query: format!("NOT ({})", sql.query),
                     params: sql.params,
-                    tables: sql.tables,
                 })
             }
             Expr::Or(left, rigth) => {
                 let mut left_sql = SqlQuery::compile_to_sql(left)?;
                 let mut rigth_sql = SqlQuery::compile_to_sql(rigth)?;
                 left_sql.params.append(&mut rigth_sql.params);
-                left_sql.tables.extend(rigth_sql.params);
                 Ok(SqlQuery {
                     query: format!("{} OR {}", left_sql.query, rigth_sql.query),
-                    tables: left_sql.tables,
                     params: left_sql.params,
                 })
             }
             Expr::Condition { field, value } => {
-                let mut tables = HashSet::new();
                 let row = match field.as_str() {
-                    "artista" => {
-                        tables.insert("performers".to_string());
-                        "performers.name"
-                    }
-                    "album" => {
-                        tables.insert("albums".to_string());
-                        "albums.name"
-                    }
+                    "artista" => "performers.name",
+                    "album" => "albums.name",
                     "año" => "rolas.year",
                     "género" => "rolas.genre",
                     "titulo" => "rolas.title",
@@ -63,7 +48,6 @@ impl SqlQuery {
                 Ok(SqlQuery {
                     query: format!("{} = ?", row),
                     params: vec![value.clone()],
-                    tables,
                 })
             }
         }
