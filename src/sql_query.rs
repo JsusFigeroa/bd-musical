0b8ac8a899429c757118601dd761d758ff693e99
Jesus Figueroa <jsusfigueroa@ciencias.unam.mx>
2026-04-30 15:05:36 -0600

    Se añaden test para la función que compila el árbol ast a sql

--- src/sql_query.rs
+++ src/sql_query.rs
@@ -13,7 +13,7 @@ impl SqlQuery {
                 let mut right_sql = SqlQuery::compile_to_sql(right)?;
                 left_sql.params.append(&mut right_sql.params);
                 Ok(SqlQuery {
-                    query: format!("{} AND {}", left_sql.query, right_sql.query),
+                    query: format!("({} AND {})", left_sql.query, right_sql.query),
                     params: left_sql.params,
                 })
             }
@@ -29,7 +29,7 @@ impl SqlQuery {
                 let mut rigth_sql = SqlQuery::compile_to_sql(rigth)?;
                 left_sql.params.append(&mut rigth_sql.params);
                 Ok(SqlQuery {
-                    query: format!("{} OR {}", left_sql.query, rigth_sql.query),
+                    query: format!("({} OR {})", left_sql.query, rigth_sql.query),
                     params: left_sql.params,
                 })
             }
@@ -51,5 +51,49 @@ impl SqlQuery {
                 })
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::expression::Expr;
+    use assert2::check;
+
+    #[test]
+    fn test_condition_sql() {
+        let search_str = r#"artista:"Boguetto""#;
+        let ast = Expr::process_str(search_str).unwrap();
+        let sql = SqlQuery::compile_to_sql(&ast).unwrap();
+        let exprected_query = "performers.name = ?".to_string();
+        let expected_params = vec!["Boguetto".to_string()];
+        check!(sql.query == exprected_query);
+        check!(expected_params == sql.params);
+    }
+
+    #[test]
+    fn test_and_sql() {
+        let search_str = r#"artista:"Boguetto" & año:"2026""#;
+        let ast = Expr::process_str(search_str).unwrap();
+        let sql = SqlQuery::compile_to_sql(&ast).unwrap();
+        let exprected_query = "(performers.name = ? AND rolas.year = ?)".to_string();
+        let exprected_params = vec!["Boguetto".to_string(), "2026".to_string()];
+        check!(sql.query == exprected_query);
+        check!(exprected_params == sql.params);
+    }
+
+    #[test]
+    fn test_multiple_condition() {
+        let search_str = r#"artista:"Boguetto" & (año:"2026" | año:"2024")"#;
+        let ast = Expr::process_str(search_str).unwrap();
+        let sql = SqlQuery::compile_to_sql(&ast).unwrap();
+        let expeted_query = "(performers.name = ? AND (rolas.year = ? OR rolas.year = ?))";
+        let exppected_params = vec![
+            "Boguetto".to_string(),
+            "2026".to_string(),
+            "2024".to_string(),
+        ];
+        check!(sql.query == expeted_query);
+        check!(sql.params == exppected_params);
     }
 }
